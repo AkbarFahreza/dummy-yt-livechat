0b44d106e158eb0a1b2f9d07cfff371ce7d51f7a
AkbarFahreza <indoc142@gmail.com>
2024-05-01 17:59:37 +0700

    Design : Fixing render button position

---
+++ src/image.js
@@ -0,0 +1,5 @@
+const BadgeImage =
+  "https://res.cloudinary.com/dxcmt3zoc/image/upload/v1680602632/Frame_2_hzxp2b.png";
+const ProfilePict =
+  "https://yt4.ggpht.com/1KqRN-uj2pOiaJqesu3o_rc2ogQmVc4RnjIf-1zTxO2OmYLPJrYbmP5K1pW6ngX9WTEXI20Q=s32-c-k-c0x00ffffff-no-rj";
+export { ProfilePict, BadgeImage };
